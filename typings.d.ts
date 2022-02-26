e08f8bd8f13c0f3a71764f3fd361c9af6099a3be
Daniel Tweedie <djtwd.ix@icloud.com>
2022-02-26 11:46:29 -0400

    Create typings

---
+++ typings.d.ts
@@ -0,0 +1,23 @@
+export interface Props {
+  posts: Post[]
+}
+
+export interface Post {
+  _id: string
+  _createdAt: string
+  title: string
+  author: {
+    name: string
+    image: string
+  }
+  description: string
+  mainImage: {
+    asset: {
+      url: string
+    }
+  }
+  slug: {
+    current: string
+  }
+  body: [object]
+}
